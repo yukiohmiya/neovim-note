606005c0bd7a6bc25d29125c6ac7eaa7d30c8903
yukiohmiya <ohmiya.yuki@gmail.com>
2019-12-17 19:00:19 +0900

    [FIX] fix error when close. and add date header if no file exists.

--- rplugin/python3/neovim-note.py
+++ rplugin/python3/neovim-note.py
@@ -1,10 +1,11 @@
 import neovim
 import datetime
+import os
 
 
 @neovim.plugin
 class NotePlugin(object):
-    notes_dir = '~/Documents/notes/'
+    notes_dir = '~/Documents/notes'
 
     def __init__(self, nvim):
         self.nvim = nvim
@@ -14,10 +15,15 @@ class NotePlugin(object):
     def note(self, args, range):
         if self.toggle:
             self.nvim.command('wincmd b')
-            self.nvim.command('wq')
+            try:
+                self.nvim.command('wq')
+            except Exception:
+                pass
         else:
             dt = datetime.date.today()
-            today_notes = '{}{}-{}-{}.md'.format(self.notes_dir, dt.year, dt.month, dt.day)
+            todays_note = '{}/{}-{}-{}.md'.format(self.notes_dir, dt.year, dt.month, dt.day)
             self.nvim.command('setlocal splitright')
-            self.nvim.command('vnew {}'.format(today_notes))
+            self.nvim.command('vnew {}'.format(todays_note))
+            if not os.path.isfile(todays_note):
+                self.nvim.current.line = ('# {}-{}-{}'.format(dt.year, dt.month, dt.day))
         self.toggle = ~self.toggle
